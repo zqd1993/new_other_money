ff4e8c05e1c3b9ff2d5127a6f2bba6e3cc779a86
qiandongzhao <987889957@qq.com>
2022-09-13 17:15:46 +0800

    commit

--- app/src/main/java/com/renyisjerunwetry/ewtfhweryy/renyirhgfhntpage/renyirhgfhntactivity/GuideRenYiHsdyjurnActivity.java
+++ app/src/main/java/com/renyisjerunwetry/ewtfhweryy/renyirhgfhntpage/renyirhgfhntactivity/GuideRenYiHsdyjurnActivity.java
@@ -107,7 +107,7 @@ public class GuideRenYiHsdyjurnActivity extends RxAppCompatActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://wentree.oss-cn-hangzhou.aliyuncs.com/94/oryjd.json")
+                            .url("https://wentree.oss-cn-hangzhou.aliyuncs.com/95/vryjd.json")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
